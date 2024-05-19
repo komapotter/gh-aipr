aa736d76478e8a6fdc2792e8697f629df42ffba3
komapotter <36rafts@gmail.com>
2024-05-19 11:37:02 +0900

    Remove unnecessary API call and print statements.

--- main.go
+++ main.go
@@ -69,20 +69,6 @@ func main() {
 		verbose = true
 	}
 
-	fmt.Println("hi world, this is the gh-aipr extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct{ Login string }{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
-
 	diffOutput, err := getGitDiff()
 	if err != nil {
 		fmt.Println("Error getting git diff:", err)
