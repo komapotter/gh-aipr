aadb635a804ebf76d96648d7d9bc6e81eb615779
komapotter <36rafts@gmail.com>
2024-06-12 12:23:38 +0900

    Refactor CreateOpenAIQuestion to support generating Japanese prompts.

--- prompt.go
+++ prompt.go
@@ -9,16 +9,57 @@ const (
 	PrBody
 )
 
-func CreateOpenAIQuestion(promptType PromptType, diffOutput string) string {
-	if promptType == PrTitle {
-		return fmt.Sprintf(`
+func CreateOpenAIQuestion(promptType PromptType, diffOutput string, japanise bool) string {
+	if japanise {
+		if promptType == PrTitle {
+			return fmt.Sprintf(`
+プルリクエストのタイトルを生成してください。
+（タイトルのみを一行で出力してください。）
+（git diffの結果を出力しないでください。）
+
+%s`, diffOutput)
+		} else if promptType == PrBody {
+			return fmt.Sprintf(`
+プルリクエストの説明を生成してください。
+（git diffの結果を出力しないでください。）
+
+以下はプルリクエスト説明フォーマットのサンプルです。
+
+---
+## 説明
+
+* 説明
+
+### 変更点
+1. 変更点:
+   - 説明
+
+2. 変更点:
+   - 説明
+
+3. 変更点:
+   - 説明
+
+...
+...
+...
+
+### テスト
+- 説明
+
+---
+%s`, diffOutput)
+		}
+	} else {
+		if promptType == PrTitle {
+			return fmt.Sprintf(`
 Please generate an appropriate pull request title based on the context.
 (Output only the title in one line.)
 (Do not output the result of git diff)
 
 %s`, diffOutput)
-	} else if promptType == PrBody {
-		return fmt.Sprintf(`
+		} else if promptType == PrBody {
+			return fmt.Sprintf(`
 Please generate an appropriate pull request description based on the context.
 (Do not output the result of git diff)
 
@@ -48,6 +89,7 @@ Here is a sample of pull-request description format.
 
 ---
 %s`, diffOutput)
+		}
 	}
 	return ""
 }
